dcda431c4f29a1ede1278d804c8a04a0f820fba5
nishant21saini <161240067+nishant21saini@users.noreply.github.com>
2024-02-26 18:21:06 +0000

    datatypes

---
+++ 01_let.js
@@ -0,0 +1,9 @@
+const accountId = 123;
+let accountMail = "saini@gmail.com";
+var accountName = "Nishant"
+
+/*
+prefer not to use var
+because of issue in block scope and functional Scope   
+*/
+console.table([accountId , accountMail, accountName]);
---
+++ 02_datatypes.js
@@ -0,0 +1,14 @@
+"use strict" ;
+/*
+DataTypes
+Number
+bigInt
+string => ""
+boolean => true/false
+null => standalone value
+undefined => 
+
+Objects 
+
+
+*/
