6c5af0c6169863af1f83670fa79439cffb7ac041
nishant21saini <161240067+nishant21saini@users.noreply.github.com>
2024-02-28 17:27:44 +0000

    change

---
+++ 03_conversion.js
@@ -0,0 +1,22 @@
+const score = "11"
+
+
+console.log(typeof(score));
+let valueInNumber = Number(score);
+console.log(typeof(valueInNumber));
+console.log(valueInNumber);
+//*************************operations**********************//
+let value =3 ;
+let negValue = -value;
+console.log(value);
+console.log(negValue);
+console.log(typeof("1" + 2));
+console.log(typeof("1" + "2"));
+console.log(1+2+"3");
+
+const id = Symbol("123");
+const anotherId = Symbol("123");
+console.log(id === anotherId);
+
+
+
